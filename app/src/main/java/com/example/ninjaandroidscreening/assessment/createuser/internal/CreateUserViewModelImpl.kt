bb8c48bea713f09eb8f8f9d315e2f16428c8cba0
Chase <chase.moon93@gmail.com>
2024-10-19 11:32:22 -0700

    The app is working until the part where you hit the button to save the user

--- app/src/main/java/com/example/ninjaandroidscreening/assessment/createuser/internal/CreateUserViewModelImpl.kt
+++ app/src/main/java/com/example/ninjaandroidscreening/assessment/createuser/internal/CreateUserViewModelImpl.kt
@@ -4,23 +4,23 @@ import androidx.lifecycle.ViewModel
 import com.example.ninjaandroidscreening.assessment.createuser.CreateUserViewModel
 import com.example.ninjaandroidscreening.assessment.createuser.personalpreferences.Interest
 import com.example.ninjaandroidscreening.assessment.createuser.personalpreferences.Pet
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class CreateUserViewModelImpl : ViewModel(), CreateUserViewModel {
 
-    override val enteredEmail: StateFlow<String?>
-        get() = TODO("Not yet implemented")
-    override val selectedInterest: StateFlow<Interest?>
-        get() = TODO("Not yet implemented")
-    override val selectedPet: StateFlow<Pet?>
-        get() = TODO("Not yet implemented")
+    override val enteredEmail = MutableStateFlow<String?>(null)
+    private val enteredPassword = MutableStateFlow<String?>(null)
+    override val selectedPet = MutableStateFlow<Pet?>(null)
+    override val selectedInterest = MutableStateFlow<Interest?>(null)
 
     override fun onPersonalDataSubmitted(email: String, password: String) {
-        TODO("STOPSHIP")
+        enteredEmail.value = email
+        enteredPassword.value = password
     }
 
     override fun onPersonalPreferencesSubmitted(pet: Pet, interest: Interest) {
-        TODO("STOPSHIP")
+        selectedPet.value = pet
+        selectedInterest.value = interest
     }
 
     override fun onCreateUserConfirmed() {
