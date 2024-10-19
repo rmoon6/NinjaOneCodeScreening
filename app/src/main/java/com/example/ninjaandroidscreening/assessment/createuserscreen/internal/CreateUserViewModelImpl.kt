5bb450c03de374d527d0ab21c9f5a1d572228aeb
Chase <chase.moon93@gmail.com>
2024-10-19 14:23:09 -0700

    Hooked up the room db to the view model

--- app/src/main/java/com/example/ninjaandroidscreening/assessment/createuserscreen/internal/CreateUserViewModelImpl.kt
+++ app/src/main/java/com/example/ninjaandroidscreening/assessment/createuserscreen/internal/CreateUserViewModelImpl.kt
@@ -1,21 +1,26 @@
 package com.example.ninjaandroidscreening.assessment.createuserscreen.internal
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ninjaandroidscreening.assessment.createuserscreen.CreateUserViewModel
 import com.example.ninjaandroidscreening.assessment.createuserscreen.personalpreferences.Interest
 import com.example.ninjaandroidscreening.assessment.createuserscreen.personalpreferences.Pet
+import com.example.ninjaandroidscreening.assessment.userdb.UserDao
+import com.example.ninjaandroidscreening.assessment.userdb.UserEntity
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CreateUserViewModelImpl : ViewModel(), CreateUserViewModel {
+class CreateUserViewModelImpl @Inject constructor(
+    private val userDao: UserDao
+) : ViewModel(), CreateUserViewModel {
 
     override val enteredEmail = MutableStateFlow<String?>(null)
-    private val enteredPassword = MutableStateFlow<String?>(null)
     override val selectedPet = MutableStateFlow<Pet?>(null)
     override val selectedInterest = MutableStateFlow<Interest?>(null)
 
     override fun onPersonalDataSubmitted(email: String, password: String) {
         enteredEmail.value = email
-        enteredPassword.value = password
     }
 
     override fun onPersonalPreferencesSubmitted(pet: Pet, interest: Interest) {
@@ -24,6 +29,14 @@ class CreateUserViewModelImpl : ViewModel(), CreateUserViewModel {
     }
 
     override fun onCreateUserConfirmed() {
-        TODO("STOPSHIP")
+        viewModelScope.launch { userDao.insertUser(requireUser()) }
+    }
+
+    private fun requireUser(): UserEntity {
+        return UserEntity(
+            email = requireNotNull(enteredEmail.value) { "Trying to create a user when entered email is null!!" },
+            pet = requireNotNull(selectedPet.value) { "Trying to create a user when selected pet is null!!" },
+            interest = requireNotNull(selectedInterest.value) { "Trying to create a user when selected interest is null!!" },
+        )
     }
 }
